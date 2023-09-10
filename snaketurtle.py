c8aac5e4d453d3fa512489c2a0a2751b4aacc323
Monkey D. Luffy <144514182+PankajGajpalla@users.noreply.github.com>
2023-09-10 12:33:24 +0530

    Add files via upload

---
+++ snaketurtle.py
@@ -0,0 +1,138 @@
+import turtle
+import random
+import time
+
+# Set up the screen
+window = turtle.Screen()
+window.title("Snake Game")
+window.bgcolor("black")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+# Snake body
+segments = []
+
+# Functions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def check_collision():
+    # Check for collision with the food
+    if head.distance(food) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        food.goto(x, y)
+
+        # Add a segment to the snake
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        segments.append(new_segment)
+
+def check_wall_collision():
+    # Check for collision with the wall
+    if (
+        head.xcor() > 290
+        or head.xcor() < -290
+        or head.ycor() > 290
+        or head.ycor() < -290
+    ):
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "Stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
+def move_segments():
+    # Move the segments in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # Move the first segment to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+# Keyboard bindings
+window.listen()
+window.onkeypress(go_up, "w")
+window.onkeypress(go_down, "s")
+window.onkeypress(go_left, "a")
+window.onkeypress(go_right, "d")
+
+# Main game loop
+while True:
+    window.update()
+
+    # Check for collision with food
+    check_collision()
+
+    # Check for collision with wall
+    check_wall_collision()
+
+    # Move the snake segments
+    move_segments()
+
+    # Move the snake
+    move()
+
+    time.sleep(0.1)
+
+window.mainloop()
